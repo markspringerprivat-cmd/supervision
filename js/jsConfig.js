e820f6c0e7e23319ec68f06c06b06e6aa8af06f9
markspringerprivat-cmd <mark.springer.privat@gmail.com>
2026-06-11 21:03:51 +0200

    delete

--- js/jsConfig.js
+++
@@ -1,6 +0,0 @@
-// Google-Sheet-Anbindung
-// Diese URL ist deine veröffentlichte Google-Apps-Script-Web-App.
-window.SUPERVISION_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwKOyddRuVQQdnuUiy8kmn1att21ivI96DXKshSH8ihYvLuIUrz0rPf8Yfbb4POgXdi/exec",
-  ADMIN_PASSWORD_HINT: "Passwort wird im Apps Script gesetzt, nicht hier."
-};
